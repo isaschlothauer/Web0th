99a7cff21cfa08a3dbbd4bd1e2a157d41002f737
Isamu Schlothauer <isamu.schlothauer@gmail.com>
2024-07-09 16:59:21 +0200

    fixed build command on root package.json

--- server/dist/src/config/cors.js
+++ server/dist/src/config/cors.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsOptions = void 0;
 exports.corsOptions = {
-    origin: `${process.env.SERVER_URL}:${process.env.F_PORT}`,
+    origin: `${process.env.SERVER_URL}${process.env.F_PORT}`,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     optionSuccessfulStatus: 200
